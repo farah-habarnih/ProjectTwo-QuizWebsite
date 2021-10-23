7fde80d2f11250bea72d857008c6361c940147e6
farah-habarnih <90200659+farah-habarnih@users.noreply.github.com>
2021-10-23 15:33:30 +0300

    Add files via upload

---
+++ source code/register.js
@@ -0,0 +1,11 @@
+//Navbar 
+
+
+
+
+
+const orders = document.getElementById('email');
+const holder_images = document.querySelector('.holder');
+const leftImage = document.getElementById('leftImage');
+const centerImage = document.getElementById('centerImage');
+const rightImage = document.getElementById('rightImage');
