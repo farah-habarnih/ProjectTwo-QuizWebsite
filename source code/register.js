370883f6b1a193de98aeca64e7fe9a8b8f055a2e
farah-habarnih <90200659+farah-habarnih@users.noreply.github.com>
2021-10-23 18:40:30 +0300

    Add files via upload

--- source code/register.js
+++ source code/register.js
@@ -1,11 +1,33 @@
 //Navbar 
 
+function register(){
+let fname = document.getElementById('fname').value;
+let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
 
 
+localStorage.setItem("fname",fname);
+localStorage.setItem("email",email);
+localStorage.setItem("password",password);
+
+}
 
 
-const orders = document.getElementById('email');
-const holder_images = document.querySelector('.holder');
-const leftImage = document.getElementById('leftImage');
-const centerImage = document.getElementById('centerImage');
-const rightImage = document.getElementById('rightImage');
+//Registration form
+
+
+// function register(){
+//     function Register(fname,email,password){
+//         this.email=email;
+//         this.fname=fname;
+//         this.password=password;
+//         Register.holder.push(this);
+//         }
+        
+//         Register.holder= [];
+        
+//         localStorage.setItem('data', JSON.stringify(Register.holder));
+        
+        
+//         console.log(Register.holder);
+// }
